fbdef0495588368c506b987856d15beecad1be6b
Jayme Anchante <jayme.anchante@disroot.org>
2020-12-13 17:08:31 -0300

    :sparkles: added put and delete methods to the api

--- resources/workday.py
+++ resources/workday.py
@@ -12,13 +12,23 @@ from model.sqlite import (
 
 def workday(date):
     values = [i["time"] for i in query_db("SELECT time FROM dates WHERE date=?", (date,))]
+    idx = len(values)
     if "api" in request.path:
         if request.method == "GET":
             return jsonify(values)
         elif request.method == "POST":
             time = request.form["time"]
-            insert_db("INSERT INTO dates (date, time) VALUES (?, ?)", (date, time))
+            insert_db("INSERT INTO dates (date, idx, time) VALUES (?, ?, ?)", (date, idx, time))
             if "client" in request.path:
                 return redirect(url_for("workday", date=date))
+            return jsonify({date: [i["time"] for i in query_db("SELECT time FROM dates WHERE date=?", (date,))] })
+        elif request.method == "DELETE":
+            id = request.form["id"]
+            insert_db("DELETE FROM dates WHERE id=?", (id,))
+            return jsonify({date: [i["time"] for i in query_db("SELECT time FROM dates WHERE date=?", (date,))] })
+        elif request.method == "PUT":
+            id = request.form["id"]
+            time = request.form["time"]
+            insert_db("UPDATE dates SET time=? WHERE id=?", (time,id,))
             return jsonify({date: [i["time"] for i in query_db("SELECT time FROM dates WHERE date=?", (date,))] })
     return render_template("index.html", date=date, values=values)
